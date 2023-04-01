d038fd0f9d67d01a5056dc936a8496d1f4d59036
Luis Vásquez <vluis5457@gmail.com>
2023-03-31 21:15:45 -0600

    Terminando practica05MVC

---
+++ Data/EquipoContext.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.EntityFrameworkCore;
+using WebMVC_PWA.Models;
+
+namespace WebMVC_PWA.Data
+{
+    public class EquipoContext : DbContext
+    {
+
+        public EquipoContext(DbContextOptions options) : base(options)
+        {
+
+        }
+
+
+        public DbSet<marcas> marcas { get; set; }   
+
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,41 @@
+using Microsoft.EntityFrameworkCore;
+using WebMVC_PWA.Data;
+
+var builder = WebApplication.CreateBuilder(args);
+
+// Add services to the container.
+builder.Services.AddControllersWithViews();
+
+
+// TODO: Agregar la conexion a la DB
+const string CONNECTION_NAME = "equiposDB";
+
+// Obtener la cadena de conexion del appsetting
+var connectionString = builder.Configuration.GetConnectionString(CONNECTION_NAME);
+
+// Agregar contexto
+builder.Services.AddDbContext<EquipoContext>(options => options.UseSqlServer(connectionString));
+
+
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+if (!app.Environment.IsDevelopment())
+{
+    app.UseExceptionHandler("/Home/Error");
+    // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
+    app.UseHsts();
+}
+
+app.UseHttpsRedirection();
+app.UseStaticFiles();
+
+app.UseRouting();
+
+app.UseAuthorization();
+
+app.MapControllerRoute(
+    name: "default",
+    pattern: "{controller=Home}/{action=Index}/{id?}");
+
+app.Run();
